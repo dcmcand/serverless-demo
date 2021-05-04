388ceb76d84819249b500afd1896206d197f9f2a
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2021-05-04 09:50:28 -0400

    and env vars to handler.py and update handler.py

--- handler.py
+++ handler.py
@@ -1,9 +1,35 @@
 import json
-
+import os
+import boto3
 
 def transfer(event, context):
+    try:
+        outbound_bucket = os.environ["OUTBOUND_BUCKET"]
+        s3 = boto3.resource('s3')
+    except Exception as e:
+        print(e)
     for record in event['Records']:
-        s3 = record['s3']
-        bucket = s3['bucket']['name']
-        key = s3["object"]['key']
-        print(f"key: {bucket}/{key}")
+        try:
+            inbound_bucket = record['s3']['bucket']['name']
+            key = s3["object"]['key']
+            print(f"Copying {inbound_bucket}/{key} to {outbound_bucket}/{key}")
+            source = {
+                'Bucket': inbound_bucket,
+                'Key': key
+            }
+
+            destination = s3.Bucket(outbound_bucket)
+            obj = destination.Object(key)
+            obj.copy(source)
+        except Exception as e:
+            print(f"An error occured copying {key} to {outbound_bucket}")
+            print(e)
+        try:
+            print(f"Deleting {inbound_bucket}/{key}")
+            object_to_delete = s3.Object(inbound_bucket, key)
+            object_to_delete.delete()
+        except Exception as e:
+            print(f"Could not delete {inbound_bucket}/{key}")
+            print(e)
+
+
